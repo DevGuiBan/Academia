99f94b688e08ecf9da6f0af973e0e6822a81d4d4
DevGuiBan <guibandeira290@gmail.com>
2024-11-04 14:16:12 -0300

    Modificando as rotas

--- academia-laravel/routes/web.php
+++ academia-laravel/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Users\AuthController;
+use App\Http\Controllers\Users\AlunoController;
+use App\Http\Controllers\Users\PersonalController;
+use App\Http\Controllers\Treino\SolicitarTreinoController;
+use App\Http\Controllers\Treino\TreinoExercicioController;
+use App\Http\Controllers\Treino\ExercicioController;
+use App\Http\Controllers\Treino\AlunoTreinoController;
+use App\Http\Controllers\Treino\TreinoController;
+use App\Http\Controllers\Treino\NotificacaoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,39 +31,74 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/logout', [AuthController::class, 'logout']);
 
     Route::prefix('personal')->group(function () {
-        Route::get('/alunos', function () {
-            return view('personal.alunos');
-        });
+        Route::get('/alunos',[SolicitarTreinoController::class,'indexAlunos'])
+        ->name('personal.alunos');
         
         Route::get('/horario', function () {
             return view('personal.horario');
         });
         
-        Route::get('/exercicio', function () {
-            return view('personal.exercicio');
-        });
+        Route::get('/{id}/exercicio', [PersonalController::class, 'getExercicios'])
+        ->name('personal.exercicios');
+
+        Route::post('/salvar-exercicio', [ExercicioController::class, 'store'])
+        ->name('personal.createExercicio');
         
-        Route::get('/salvar-exercicio', function () {
-            return view('personal.salvarExercicio');
-        });
+        Route::post('/salvar-exercicio/{treino_id}/{exercicio_id}', [TreinoExercicioController::class, 'salvarExercicio'])
+        ->name('personal.salvarExercicio');
         
-        Route::get('/salvar-treino', function () {
-            return view('personal.salvarTreino');
-        });
+        Route::get('/salvar-exercicio',[ExercicioController::class, 'index'])
+        ->name('personal.exercicio');
+
+        Route::post('/selecionar-exercicios/{aluno_id}/{personal_id}/{treino_id}', [TreinoExercicioController::class, 'store'])
+        ->name('personal.salvarTreino');
+
+        Route::get('/selecionar-exercicios/{aluno_id}/{personal_id}/{treino_id}', [TreinoExercicioController::class, 'selecionarExercicios'])
+        ->name('personal.selecionarExercicios');
+
+        Route::get('/treino/create',[TreinoController::class,'index'])
+        ->name('personal.criarTreino');
+
+        Route::post('/treino/create/{id}',[TreinoController::class,'store'])
+        ->name('personal.create');
+
+        Route::put('/treino/edit/{id}', [AlunoTreinoController::class, 'edit']
+        )->name('personal.editTreino');
         
-        Route::get('/treino', function () {
-            return view('personal.treino');
-        });
+        Route::get('/treino', [SolicitarTreinoController::class, 'index']
+        )->name('personal.treino');
+
+        Route::post('/notificacao/{id}/lida', [NotificacaoController::class, 'marcarComoLida'])
+        ->name('notificacao.lida');
+
+        Route::get('/profile/{id}',[PersonalController::class,'profile'])
+        ->name('personal.profile');
+
+        Route::delete('/profile/{id}',[PersonalController::class,'destroy'])
+        ->name('personal.delete');
+
+        Route::put('/profile/{id}',[PersonalController::class,'update'])
+        ->name('personal.update');
     });
 
     Route::prefix('aluno')->group(function () {
-        Route::get('/treino', function () {
-            return view('aluno.treino');
-        });
+        Route::get('/treino', [AlunoTreinoController::class,'index'])
+        ->name('aluno.treino');
+
+        Route::get('/profile/{id}',[AlunoController::class,'profile'])
+        ->name('aluno.profile');
+
+        Route::put('/profile/{id}',[AlunoController::class,'update'])
+        ->name('aluno.update');
+
+        Route::delete('/profile/{id}',[AlunoController::class,'destroy'])
+        ->name('aluno.delete');
         
-        Route::get('/solicitar-treino', function () {
-            return view('aluno.solicitarTreino');
-        });
+        Route::get('/solicitar-treino', [SolicitarTreinoController::class, 'indexCreateSolicities'])
+        ->name('aluno.solicitarTreino');
+
+        Route::post('/solicitar-treino/{id}',[SolicitarTreinoController::class,'store'])
+        ->name('solicitar-treino');
         
         Route::get('/progresso', function () {
             return view('aluno.progresso');
